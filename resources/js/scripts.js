e17bd76f637bec737d0779250044ab6c26bb8a81
Crystal <hello@crystalbohacek.com>
2019-03-04 22:54:04 -0500

    added publisher info

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
 var value = '';
-var apikey = config.API_KEY;
 
 $( "input" )
   .keyup(function() {
@@ -10,7 +9,7 @@ $( "input" )
   .keyup();
 
 $( "button" ).click(function() {
-    $.getJSON( "https://www.googleapis.com/books/v1/volumes?q=" + value + "&key=" + apikey, function( data ) {
+    $.getJSON( "https://www.googleapis.com/books/v1/volumes?q=" + value + "&key=AIzaSyA_0WhuTJiTIrRRbFVvMB05FjzhuL-Yeng", function( data ) {
 	  var items = data.items; 
 	  var html = '';
 	  $(".results").text("displaying results for \"" + value + "\"");
@@ -18,17 +17,15 @@ $( "button" ).click(function() {
 	  for (i = 0; i<=items.length-1; i++){
 		var newLine = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-3"><div class="book-info bg-white">' + 
 		(!!items[i].volumeInfo.imageLinks && '<img src="' + items[i].volumeInfo.imageLinks.thumbnail + '">' ) +
-		'<p class="mt-3"><span class="book-title">' + items[i].volumeInfo.title + '</span> by ' +
-		(!!items[i].volumeInfo.authors ? items[i].volumeInfo.authors[0] : "unknown author") + '</p>' +
+		'<p class="mt-3"><span class="book-title">' + items[i].volumeInfo.title + '</span></p><p> by ' +
+		(!!items[i].volumeInfo.authors ? items[i].volumeInfo.authors[0] : "unknown author") + '</p>' + '<p class="publisher">' +
+		(!!items[i].volumeInfo.publisher ? items[i].volumeInfo.publisher : "unknown publisher") + '</p>' + 
 		(!!items[i].volumeInfo.infoLink && '<a class="book-link btn btn-success" href="' + items[i].volumeInfo.infoLink + '">See this book</a></div></div>');
 
 	  	html += newLine;
 	  }
 	  $(".results-list").html(html);
 
-	  // $('.book-info img').each(function(){
-	  // 	var imgSrc = $(this).prop('src').replace('zoom=1', 'zoom=2');
-	  // });
   });
 });
 
