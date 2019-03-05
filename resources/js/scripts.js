29d34f938b60c816e51ade8ced2d861b0c97ee38
Crystal <hello@crystalbohacek.com>
2019-03-04 22:37:11 -0500

    added styling

--- resources/js/scripts.js
+++ resources/js/scripts.js
@@ -13,13 +13,22 @@ $( "button" ).click(function() {
     $.getJSON( "https://www.googleapis.com/books/v1/volumes?q=" + value + "&key=" + apikey, function( data ) {
 	  var items = data.items; 
 	  var html = '';
-	  $(".results").text(items.length + " results for \"" + value + "\"");
+	  $(".results").text("displaying results for \"" + value + "\"");
 
 	  for (i = 0; i<=items.length-1; i++){
-	  	var newLine = '<li>' + items[i].volumeInfo.title + '</li>';
+		var newLine = '<div class="col-lg-3 col-md-4 col-sm-6 col-xs-12 mb-3"><div class="book-info bg-white">' + 
+		(!!items[i].volumeInfo.imageLinks && '<img src="' + items[i].volumeInfo.imageLinks.thumbnail + '">' ) +
+		'<p class="mt-3"><span class="book-title">' + items[i].volumeInfo.title + '</span> by ' +
+		(!!items[i].volumeInfo.authors ? items[i].volumeInfo.authors[0] : "unknown author") + '</p>' +
+		(!!items[i].volumeInfo.infoLink && '<a class="book-link btn btn-success" href="' + items[i].volumeInfo.infoLink + '">See this book</a></div></div>');
+
 	  	html += newLine;
 	  }
 	  $(".results-list").html(html);
+
+	  // $('.book-info img').each(function(){
+	  // 	var imgSrc = $(this).prop('src').replace('zoom=1', 'zoom=2');
+	  // });
   });
 });
 
